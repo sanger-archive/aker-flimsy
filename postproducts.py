37fb71a16a593c083f39279496447952e31b5c57
David Robinson <dr6@sanger.ac.uk>
2017-03-17 12:30:45 +0000

    Added postproducts script and text catalogue file

---
+++ postproducts.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python -tt
+
+import argparse
+import requests
+import json
+
+DEFAULT_PROXY = 'wwwcache.sanger.ac.uk:3128'
+HEADERS = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+
+class Product(object):
+    def __init__(self):
+        self.name = None
+        self.version = None
+        self.desc = None
+        self.unit_cost = None
+
+def build_data(filename):
+    product = None
+    catalog_data = {'products':[]}
+    with open(filename, 'r') as fin:
+        for line in fin:
+            line = line.strip()
+            if not line:
+                continue
+            if line.lower()=='product':
+                product = {}
+                catalog_data['products'].append(product)
+                continue
+            k,v = line.split(':', 1)
+            k = k.strip()
+            v = v.strip()
+            if product is None:
+                catalog_data[k] = v
+            else:
+                product[k] = v
+    return catalog_data
+
+
+def send_request(data, url, proxy):
+    proxies = { 'http': proxy, 'https': proxy } if proxy else {}
+    r = requests.post(url=url, data=data, proxies=proxies, headers=HEADERS)
+    print r.status_code
+                
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--aker', '-a', metavar='AKER_URL')
+    parser.add_argument('--file', '-f', metavar='FILENAME', required=True)
+    parser.add_argument('--proxy', '-p', metavar='PROXY', default=DEFAULT_PROXY)
+    args = parser.parse_args()
+    req = build_data(args.file)
+    print json.dumps(req, indent=4)
+    send_request(json.dumps(req), args.aker, args.proxy)
+    
+if __name__ == '__main__':
+    main()
